dfab7b7cdc125a32bf7e816ea1c8e5652661e3d0
ram4ik <ram4ik@hotmail.com>
2023-10-20 11:36:48 -0700

    Initial Commit

---
+++ HowToImplementInAppTips/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowToImplementInAppTips
+//
+//  Created by admin on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowToImplementInAppTips/HowToImplementInAppTipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToImplementInAppTipsApp.swift
+//  HowToImplementInAppTips
+//
+//  Created by admin on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HowToImplementInAppTipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
